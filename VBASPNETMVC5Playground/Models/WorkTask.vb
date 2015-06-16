f1bf09da95719247ffb853020cab1b2ea48f78a6
Humayun Shabbir <humayun_shabbir@hotmail.com>
2015-06-16 06:18:44 +0500

    added WorkTask class in models

---
+++ VBASPNETMVC5Playground/Models/WorkTask.vb
@@ -0,0 +1,20 @@
+﻿Imports System.ComponentModel.DataAnnotations
+Imports System.Data.Entity
+
+Public Class WorkTask
+
+    Public Property Id As Integer
+    <Required>
+    Public Property Title As String
+    Public Property Description As String
+
+End Class
+
+
+Public Class MainDbContext
+    Inherits DbContext
+
+
+    Public Property WorkTasks As List(Of WorkTask)
+
+End Class
